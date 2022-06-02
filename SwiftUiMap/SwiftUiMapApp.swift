d782bf051a8f2ee8a3152b2549e49102a45f6d90
Etinge Mabian <etingemabian@gmail.com>
2022-06-02 15:21:38 +0100

    Initial Commit

---
+++ SwiftUiMap/SwiftUiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMapApp.swift
+//  SwiftUiMap
+//
+//  Created by Adwa Sarl on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
